e9f6661b45f74851947407be61ad3a02d032f797
Artur Mokrosiński <mokrosinski95@gmail.com>
2017-03-11 20:26:01 +0100

    Dodanie funkcji wyswietl w L1_1

--- C 2 sem/L1_1/L1_1/main.c
+++ C 2 sem/L1_1/L1_1/main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define M 5
+#define N 5
+#define MIN 1
+#define MAX 9
+
+void wyswietl(int tab[]);
+
 
 int main() {
+	//srand(time(NULL));
 
-	printf("Witaj Œwiecie!\n");
+	int tab[25] ={0,1,0,0,0,
+				0,0,1,0,1,
+				0,0,0,1,0,
+				0,0,1,0,1,
+				0,1,0,0,0 };
+
+	wyswietl(tab);
+
+
 	system("pause");
-
 	return 0;
+}
+
+void wyswietl(int tab[]){
+	for (int i = 0; i < M*N; i++)
+		printf((i + 1) % N == 0 ? "%d\n" : "%d ", tab[i]);
 }
