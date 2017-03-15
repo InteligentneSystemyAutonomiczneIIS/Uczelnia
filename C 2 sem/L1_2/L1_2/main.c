182d93bc7c19a156e4ff81ce471fa391d2116832
Artur Mokrosiński <mokrosinski95@gmail.com>
2017-03-15 14:31:40 +0100

    aktualizacja

--- C 2 sem/L1_2/L1_2/main.c
+++ C 2 sem/L1_2/L1_2/main.c
@@ -1,39 +1,86 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-#define N 5 //iloœæ wczytanych napisów
-#define L 80 //d³ugoœæ ci¹gu
 
-void wczytaj(char tab[N][L]);
-void wyswietl(char tab[N][L]);
-void sortuj(char tab[N][L]);
+int zliczanie_malych_liter(char tab[][50], int j);
+void sortowanie(char tab_napisy[][50], int tab_male_litery[], int n);
+void wypisywanie_tablicy(char tab[][50], int n);
 
-int main() {
-	char tab[N][L];
-	
-	wczytaj(tab);
-
+int main()
+{
+	int i, n, tab_male_litery[10];
+	char tab[10][50];
+	printf("Program wczytujacy n napisow podanych przez uzytkownika.\n");
 
 
+	do {
+		printf("Podaj ilosc napisow: ");
+		scanf("%d", &n);
+		fflush(stdin);
+	} while (n >= 10 || n <= 0);
+	fflush(stdin);
+	for (i = 0; i <= n; i++)
+	{
+		printf("\nPodaj %d napis: ", i );
+		gets(tab[i]);
+		tab_male_litery[i] = zliczanie_malych_liter(tab, i);
+	}
 
-	printf("\n\n\n");
+	sortowanie(tab, tab_male_litery, n);
+	printf("\nNapisy posortowane malejaco wzgledem liczby malych liter:\n");
+	wypisywanie_tablicy(tab, n);
 	system("pause");
 	return 0;
 }
 
-void wczytaj(char tab[N][L]){
-	for (int i = 0; i < N, i++)
-		fgets(tab[i], L + 1, stdin);
+
+
+
+
+
+int zliczanie_malych_liter(char tab[][50], int j)
+{
+	int i = 0;
+	int male_litery = 0;
+
+	while (tab[j][i] != '\0')
+	{
+		if (islower(tab[j][i]))
+			male_litery++;
+		i++;
+	}
+	return male_litery;
 }
 
-void wyswietl(char tab[N][L]){
-	for (int i = 0; i < N, i++)
-		printf("%c", tab[i]);
+
+
+
+void sortowanie(char tab_napisy[][50], int tab_male_litery[], int n)
+{
+	int i, j, k, tmp;
+	char tmp_napisy[50];
+	for (i = 0; i<n; i++)
+	{
+		k = i;
+		for (j = i + 1; j<n; j++)
+		{
+			if (tab_male_litery[j]>tab_male_litery[k])
+				k = j;
+		}
+		tmp = tab_male_litery[i];
+		tab_male_litery[i] = tab_male_litery[k];
+		tab_male_litery[k] = tmp;
+		strcpy(tmp_napisy, tab_napisy[i]);
+		strcpy(tab_napisy[i], tab_napisy[k]);
+		strcpy(tab_napisy[k], tmp_napisy);
+	}
 }
 
-void sortuj(char tab[N][L]){
-	for(int i=0,i<N;i++)
-		
+void wypisywanie_tablicy(char tab[][50], int n)
+{
+	int i;
+	for (i = 0; i<n; i++)
+		printf("Tab[%d] = %s\n", i, tab[i]);
 }
+
